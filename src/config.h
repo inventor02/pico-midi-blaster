51811dd5cabfc3ee7693061dfd64c99eedd0527f
George Peppard <gjp1g21@soton.ac.uk>
2023-03-28 17:20:13 +0100

    it works!

--- src/config.h
+++ src/config.h
@@ -10,6 +10,10 @@ static const uint GPIO_SD_SCK = 10;
 static const uint GPIO_SD_SS = 15;
 static const uint BAUD_SD = 30 * 1000 * 1000;
 
+static const uint GPIO_PWM_BUZZER = 18;
+
+static const uint PWM_TOP = 1000000UL;
+
 static const char MIDI_FILE_NAME[] = "midi.mid";
 
 #endif
--- src/main.c
+++ src/main.c
@@ -2,6 +2,8 @@
 
 #include "pico/stdlib.h"
 #include "pico/mutex.h"
+#include "hardware/clocks.h"
+#include "hardware/pwm.h"
 #include "hw_config.h"
 #include "sd_card.h"
 #include "ff.h"
@@ -9,14 +11,38 @@
 
 #include "config.h"
 #include "midi.h"
+#include "music.h"
+
+static uint pwm_slice;
+static uint pwm_chan;
+
+void play_note(float freq, float vol) {
+  uint32_t wrap = PWM_TOP / freq;
+  pwm_set_wrap(pwm_slice, wrap);
+
+  uint16_t duty = (wrap + 1) * vol - 1;
+  pwm_set_chan_level(pwm_slice, pwm_chan, duty);
+
+  printf("wrap=%d, duty=%d\n", wrap, duty);
+  pwm_set_enabled(pwm_slice, true);
+}
+
+void stop_note() {
+  pwm_set_enabled(pwm_slice, false);
+}
 
 int main() {
   stdio_init_all();
+
   gpio_init(PICO_DEFAULT_LED_PIN);
   gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
   gpio_put(PICO_DEFAULT_LED_PIN, 1);
 
-  printf("Hello!\n\n");
+  gpio_set_function(GPIO_PWM_BUZZER, GPIO_FUNC_PWM);
+  pwm_slice = pwm_gpio_to_slice_num(GPIO_PWM_BUZZER);
+  pwm_chan = pwm_gpio_to_channel(GPIO_PWM_BUZZER);
+
+  pwm_set_clkdiv(pwm_slice, clock_get_hz(clk_sys) / PWM_TOP);
 
   sd_card_t *sd = sd_get_by_num(0);
   mutex_init(&sd->spi->mutex);
@@ -42,8 +68,26 @@ int main() {
 
   midi_file_t midi;
   midi_file_parse(&midi, (uint8_t *)buf, 114);
-  printf("main returned\n");
+  printf("start playing\n");
 
+  uint16_t ms_per_qn = 429; // TODO get this from the MIDI
+
+  midi_event_t *events = &(midi.chunks[0].events);
+  uint8_t event_count = midi.chunks[0].length;
+
+  for (int i = 0; i < event_count; i++) {
+    busy_wait_ms((events[i].delta_time / midi.division) * ms_per_qn);
+    
+    if (events[i].type == NOTE_ON) {
+      uint8_t note = events[i].data[0];
+      printf("Playing note %d, %f\n", note, MIDI_FREQS[note]);
+      play_note(MIDI_FREQS[note], 0.05);
+    } else {
+      stop_note();
+    }
+  }
+
+  printf("done!");
   gpio_put(PICO_DEFAULT_LED_PIN, 0);
   for (;;) tight_loop_contents();
 }
